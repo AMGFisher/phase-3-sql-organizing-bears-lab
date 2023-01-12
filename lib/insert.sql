6aacce0abe5dc472c817d86b4c44df3e047b02d5
Adam Fisher <amgfisher90@gmail.com>
2023-01-12 07:44:23 -0500

    commit tree dee7d6ed17b5420623c646f08d1c24df4e99e0b2

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+('Mr Chocolate', 4, 'M', 'Brown', 'Nice', true),
+('Rowdy', 7, 'M', 'Yelllow', 'Mean', true),
+('Tabitha', 14, 'F', 'Black', 'Nice', true),
+('Sergeant Brown', 44, 'M', 'Brown', 'Mean', false),
+('Melissa', 21, 'F', 'Yellow', 'Nice', false),
+('Grinch', 17, 'M', 'Black', 'Mean', true),
+('Wendy', 3, 'F', 'Brown', 'Nice', true),
+(NULL, NULL, 'M', 'Brown', NULL, true);
+
